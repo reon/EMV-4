bd3d628d3755e027d04eada7e4d1ba3ca68f036a
Alexander McNurlan <alexandermcnurlan@isti.com>
2017-06-14 16:50:05 -0400

    FDSN Dialog and URL assembly.

--- EMV/Dialogs/fdsnrequestdialog.cpp
+++ EMV/Dialogs/fdsnrequestdialog.cpp
@@ -8,7 +8,6 @@ FDSNRequestDialog::FDSNRequestDialog(QWidget *parent) :
     ui->setupUi(this);
 
     this->setAttribute(Qt::WA_DeleteOnClose);
-//    ui->ServiceSelect->addItems(QStringList{"Event", "Station", "Dataselect"});
 }
 
 FDSNRequestDialog::~FDSNRequestDialog()
@@ -16,7 +15,66 @@ FDSNRequestDialog::~FDSNRequestDialog()
     delete ui;
 }
 
+//Add error checking function
+
 void FDSNRequestDialog::on_ConnectButton_clicked()
 {
-    ui->ConnectButton->setDisabled(true);
+//    ui->ConnectButton->setDisabled(true);
+
+    QUrl url = GenerateURL();
+    url.setQuery(GenerateQuery());
+
+    QMessageBox::information(this, "Result", url.toString());
 }
+
+void FDSNRequestDialog::on_StandardCallingPatternCheck_toggled(bool checked)
+{
+    ui->ServiceSelect->setEnabled(checked);
+    ui->MethodSelect->setEnabled(checked);
+}
+
+QUrl FDSNRequestDialog::GenerateURL()
+{
+//    QUrl url(ui->HostListWidget->currentItem()->text());
+
+    QUrl url = QUrl::fromUserInput(ui->HostListWidget->currentItem()->text());
+
+    if (ui->StandardCallingPatternCheck->isChecked()) {
+        url.setPath("/fdsnws/" + ui->ServiceSelect->currentText().toLower()
+            + "/" + ui->VersionEdit->text() + "/");
+    }
+
+    return url;
+}
+
+QString FDSNRequestDialog::GenerateQuery()
+{
+    QString Remove = "Remove";
+
+    QMap<QString, QString> fragments
+    {
+        { "minmag", ui->MagnitudeMinimumEdit->text() },
+        { "maxmag", ui->MagnitudeMaximumEdit->text() },
+        { "minlat", ui->LatitudeMinimumEdit->text() },
+        { "maxlat", ui->LatitudeMaximumEdit->text() },
+        { "minlon", ui->LongitudeMinimumEdit->text() },
+        { "maxlon", ui->LongitudeMaximumEdit->text() }
+    };
+
+    //Remove empty pairs
+    for (auto key : fragments.uniqueKeys()) {
+        qDebug() << key << "\n";
+        if (fragments.value(key).isEmpty())
+            fragments.remove(key);
+    }
+
+    QString query;
+
+
+    for (auto i = fragments.begin(); i != fragments.end(); i++) {
+        query += i.key() + "=" + i.value() + "&";
+    }
+
+    return query;
+}
+
--- EMV/Dialogs/fdsnrequestdialog.h
+++ EMV/Dialogs/fdsnrequestdialog.h
@@ -1,7 +1,14 @@
 #ifndef FDSNREQUESTDIALOG_H
 #define FDSNREQUESTDIALOG_H
 
+#include <QMessageBox>
+#include <QDebug>
+#include <QString>
+#include <QStringList>
+#include <QMap>
 #include <QDialog>
+
+#include <QUrl>
 
 namespace Ui {
 class FDSNRequestDialog;
@@ -18,8 +25,21 @@ public:
 private slots:
     void on_ConnectButton_clicked();
 
+    void on_StandardCallingPatternCheck_toggled(bool checked);
+
+private:
+    /// Return Scheme + Authority + Path
+    QUrl GenerateURL();
+    QString GenerateQuery();
+
+
+    //Members
+public:
+
 private:
     Ui::FDSNRequestDialog *ui;
+
+
 };
 
 #endif // FDSNREQUESTDIALOG_H
